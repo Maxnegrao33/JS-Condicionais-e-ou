9b8e8b26e7c22138b8308ea8805e0f7160c7622e
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2022-11-28 09:38:46 -0300

    Add files via upload

---
+++ Multi Condicionais.js
@@ -0,0 +1,13 @@
+let idade = 90;
+
+if(idade >= 18 && idade <60) {
+//O termo idade < 60 precisa ser declarado pois cada condicional é individual
+        console.log("Você é um adulto");
+
+//if(idade >= 18 || idade <60) {
+//Aqui pode escolher entre 18 ou 60
+}
+//Podemos inserir várias condicionais em declarações
+
+
+
